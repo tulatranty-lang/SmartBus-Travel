32c79ba6f8a675cec9304a2e43e8ed779d0299ae
tulatranty-lang <tulatranty@gmail.com>
2026-06-07 01:04:08 +0700

    Update API base URL for LAN demo

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,1 +1,6 @@
-window.SmartBusConfig = Object.freeze({ API_BASE_URL: window.SMARTBUS_API_BASE || "http://localhost:5000/api/v1", MAP_DEFAULT_CENTER: [16.047,108.206], MAP_DEFAULT_ZOOM: 8, REQUEST_TIMEOUT_MS: 12000 });
+window.SmartBusConfig = Object.freeze({
+  API_BASE_URL: window.SMARTBUS_API_BASE || "http://192.168.1.34/api/v1",
+  MAP_DEFAULT_CENTER: [16.047, 108.206],
+  MAP_DEFAULT_ZOOM: 8,
+  REQUEST_TIMEOUT_MS: 12000,
+});
